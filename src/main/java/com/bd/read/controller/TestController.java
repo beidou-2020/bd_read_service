a00880ba3a2be89f292fa8f3ccf17e9fc2fc88a9
beidou <18513032646@163.com>
2020-11-19 22:02:08 +0800

    服务健康检查接口修改

--- src/main/java/com/bd/read/controller/TestController.java
+++ src/main/java/com/bd/read/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequestMapping(value = "/test")
+@RequestMapping(value = "/")
 public class TestController {
 
 
