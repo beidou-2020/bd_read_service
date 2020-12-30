c587c78097434b08ff7b8a6a00f7cdae1dda74f7
beidou <18513032646@163.com>
2020-12-30 13:36:29 +0800

    部署：本地生产环境去除consul监控

--- src/main/java/com/bd/read/BdReadServiceApplication.java
+++ src/main/java/com/bd/read/BdReadServiceApplication.java
@@ -3,10 +3,12 @@ package com.bd.read;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.bd.read"})
 //扫描mapper接口
 @MapperScan(value = "com.bd.read.repository")
+@EnableTransactionManagement    //开启事务注解
 //@EnableDiscoveryClient          // 服务提供方
 public class BdReadServiceApplication {
 
